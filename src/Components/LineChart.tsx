b5aecbab202eb5f5732f902bc71c5a2826ef5555
Filip <filipradovic@protonmail.com>
2021-03-27 17:24:38 +0100

    Added charts

---
+++ src/Components/LineChart.tsx
@@ -0,0 +1,45 @@
+import ReactApexChart from 'react-apexcharts'
+
+const LineChart = (props) => {
+    const state = {
+      series: [{
+          name: "Desktops",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+      }],
+      options: {
+        chart: {
+          height: 350,
+          type: 'line',
+          zoom: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'straight'
+        },
+        title: {
+          text: 'Product Trends by Month',
+          align: 'left'
+        },
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+          },
+        },
+        xaxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+        }
+      },
+    };
+    return (
+      <div id="chart" className="col-6">
+        <ReactApexChart options={state.options} series={state.series} type="line"/>
+      </div>
+    );
+};
+
+export default LineChart;
--- src/Pages/DashboardPage.tsx
+++ src/Pages/DashboardPage.tsx
@@ -4,30 +4,14 @@ import JobList from "../Components/JobList"
 import Category from "../Components/Category"
 import Dashboard from "../Components/Dashboard"
 import ScrollToTop from "../Components/ScrollToTop";
+import StatisticsPage from "./StatisticsPage"
 import {useState} from "react"
 
 export const DashboardPage = () => {
   const [page, setPage] = useState(0);
-  const categoryClassNames = {
-    category:"media bg-white rounded-4 pl-8 pt-9 pb-9 pr-7 hover-shadow-1 mb-9 shadow-8",
-    icon:"text-orange bg-orange-opacity-1 circle-56 font-size-6 mr-7",
-    text:"font-size-8 font-weight-semibold text-black-2 line-height-reset font-weight-bold mb-1",
-    subtext:"font-size-4 font-weight-normal text-gray mb-0"
-  }
   const renderPage = () => {
     if(page === 0){
-      return (
-        <div className="row mb-7">
-          <div className="col-xxl-3 col-xl-4 col-lg-6 col-sm-6">
-            <Category
-              classNames = {categoryClassNames}
-              icon = "fas fa-mouse-pointer"
-              text = "18.6%"
-              subText = "Applied Rate"
-            />
-          </div>
-        </div>
-      );
+      return <StatisticsPage />;
     } else if (page===1) {
       return (
         <div className="mb-14">
