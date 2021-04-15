bb25d0f7cc14fcba62fd01453fad5ee5479747c9
Danijel Bošnjak <danijel.bosnjak@coreline.agency>
2021-04-15 19:22:34 +0200

    no message

--- src/Pages/PostCreatePage/PostCreatePage.tsx
+++ src/Pages/PostCreatePage/PostCreatePage.tsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
-// import '../RichEditor.css';
 import CheckBoxList from './Components/CheckBoxList';
 import RichEditor from './Components/RichEditor';
 import ReactSelect from './Components/ReactSelect';
 import TagInput from './Components/TagInput';
-import TaskTable from './Components/TaskTable';
-import ReactSlider from '../PostListPage/Components/ReactSlider';
-import { styled } from '../../utils/css';
+import ReactSlider from './Components/ReactSlider';
 import { Form } from 'antd';
 
 export const PostCreatePage = (props) => {
@@ -46,63 +43,145 @@ export const PostCreatePage = (props) => {
                   <h3 className="font-size-6 mb-0">Create Post</h3>
                 </div>
                 <div className="pt-9 pl-sm-9 pl-5 pr-sm-9 pr-5 pb-8 border-bottom border-width-1 border-default-color light-mode-texts">
-                  <div className="row pl-5 pr-5">
-                    <div className="col-8">
-                      <ReactSlider />
-                    </div>
-                    <div className="pb-10 col-6">
-                      <h4 className="font-size-6 font-weight-semibold mb-6">Currency</h4>
-                      <div className="pl-0 col-10">
-                        <ReactSelect />
+                  <div className="row">
+                    <div className="col-12">
+                    <Form
+                      name="create_post"
+                      onFinish={onFinish}
+                    >
+                      <Form.Item
+                        name="jobType"
+                        hasFeedback
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please select your job type!',
+                          },
+                        ]}
+                      >
+                        <CheckBoxList name="Job Type" items={jobTypes} />                        
+                      </Form.Item>
+                      <div className="pl-0 col-8">
+                      <Form.Item
+                        name="salaryRange"
+                        hasFeedback
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please select your salary range!',
+                          },
+                        ]}
+                      >
+                        <ReactSlider />
+                        </Form.Item>
                       </div>
-                    </div>
-                    <div className="pb-10 col-6">
-                      <h4 className="font-size-6 font-weight-semibold mb-6">Company Headquaters</h4>
-                      <div className="pl-0 col-10">
+                      <div className="pl-0 pb-10 col-6">
+                      <Form.Item
+                        name="currency"
+                        hasFeedback
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please select your currency!',
+                          },
+                        ]}
+                      >
+                        <h4 className="font-size-6 font-weight-semibold mb-6">Currency</h4>
                         <ReactSelect />
+                        </Form.Item>
                       </div>
-                    </div>
-                    <div className="pb-10 col-6">
-                      <h4 className="font-size-6 font-weight-semibold mb-6">Company Headquaters</h4>
-                      <div className="pl-0 col-10">
+                      <Form.Item
+                        name="expLevel"
+                        hasFeedback
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please select your experience level!',
+                          },
+                        ]}
+                      >
+                        <CheckBoxList name="Experience Level" items={experienceTypes} />
+                      </Form.Item>
+                      <div className="pl-0 pb-10 col-6">
+                      <Form.Item
+                        name="jobLoc"
+                        hasFeedback
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please select your job location!',
+                          },
+                        ]}
+                      >
+                        <h4 className="font-size-6 font-weight-semibold mb-6">Job Location</h4>
                         <ReactSelect />
+                        </Form.Item>
                       </div>
-                    </div>
-                    <div className="pb-10 col-6">
-                      <h4 className="font-size-6 font-weight-semibold mb-6">Job Location</h4>
-                      <div className="pl-0 col-10">
-                        <ReactSelect />
-                      </div>
-                    </div>
-                    <div className="pb-10 col-6">
-                      <h4 className="font-size-6 font-weight-semibold mb-6">Soft skills</h4>
-                      <div className="pl-0 col-10">
+                      <div className="pl-0 pb-10 col-6">
+                      <Form.Item
+                        name="softSkills"
+                        hasFeedback
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please select your soft skills!',
+                          },
+                        ]}
+                      >
+                        <h4 className="font-size-6 font-weight-semibold mb-6">Soft skills</h4>
                         <TagInput />
+                        </Form.Item>
                       </div>
-                    </div>
-                    <div className="pb-10 col-6">
-                      <h4 className="font-size-6 font-weight-semibold mb-6">Tehnical skills</h4>
-                      <div className="pl-0 col-10">
+                      <div className="pl-0 pb-10 col-6">
+                      <Form.Item
+                        name="techSkills"
+                        hasFeedback
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please select your technical skills!',
+                          },
+                        ]}
+                      >
+                        <h4 className="font-size-6 font-weight-semibold mb-6">Technical skills</h4>
                         <TagInput />
+                        </Form.Item>
                       </div>
-                    </div>
-                    <div className="pb-20 col-12">
-                      <h4 className="font-size-6 font-weight-semibold mb-6">Tips for solving tasks</h4>
-                      <RichEditor />
-                    </div>
-                    <div className="pb-20 col-12">
-                      <TaskTable />
-                    </div>
-                    <div className="pb-10 col-12">
-                      <h4 className="font-size-6 font-weight-semibold mb-6">Job Description</h4>
-                      <RichEditor />
-                    </div>
-                    <div className="pb-10 col-12">
-                      <h4 className="font-size-6 font-weight-semibold mb-6">Your Role</h4>
-                      <RichEditor />
-                    </div>
-                    <div className="col-12 my-15">
-                      <a className="btn btn-primary btn-xl w-10 text-uppercase"><span className="mr-5 d-inline-block">+</span>Create Post</a>
+                      <div className="pb-10">
+                      <Form.Item
+                        name="jobDesc"
+                        hasFeedback
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please select your job description!',
+                          },
+                        ]}
+                      >
+                        <h4 className="font-size-6 font-weight-semibold mb-6">Job Description</h4>
+                        <div className="col-12 pl-0">
+                          <RichEditor />
+                        </div>
+                        </Form.Item>
+                      </div>
+                      <div className="pb-10">
+                      <Form.Item
+                        name="yourRole"
+                        hasFeedback
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please select your job description!',
+                          },
+                        ]}
+                      >
+                        <h4 className="font-size-6 font-weight-semibold mb-6">Your Role</h4>
+                        <div className="col-12 pl-0">
+                          <RichEditor />
+                        </div>
+                        </Form.Item>
+                      </div>
+                    </Form>
                     </div>
                   </div>
                 </div>
