cfdda4a8ede528eb648a1d75d85446758215f99f
Danijel Bošnjak <danijel.bosnjak@coreline.agency>
2021-05-11 16:43:42 +0200

    company form improved

--- src/Pages/CompanyCreatePage/Components/CompanyForm.tsx
+++ src/Pages/CompanyCreatePage/Components/CompanyForm.tsx
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom';
 import RichEditor from './RichEditor';
 import ReactSelect from './ReactSelect';
 import ImageUploader from "react-images-upload";
+import { FormProvider, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/dist/yup';
+import * as yup from 'yup';
+import { TextField } from '../../../Components/TextField';
+import FormEditor from '../../../Components/Form/FormEditor';
 
 const CompanyForm = (props) => {
   const [picture, setPicture] = useState([]);
@@ -16,6 +21,25 @@ const CompanyForm = (props) => {
     }
   }
 
+  const onSubmit = (values: any) => {
+    console.log(values)
+  }
+
+  const schema = yup.object().shape({
+    name: yup.string().required('This is a required field.'),
+    size: yup.string().required('This is a required field.'),
+    address: yup.string().required('This is a required field.'),
+    founded: yup.string().required('This is a required field.'),
+  });
+
+  const methods = useForm({
+    // defaultValues: {
+    //   name: props.postState !== null ? props.postState.position : '',
+    //   size: props.postState !== null ? props.postState.experience : '',
+    //   address: props.postState !== null ? props.postState.description : ''
+    // },
+    resolver: yupResolver(schema),
+  });
   const onDrop = (picture) => {
     setPicture(picture);
   };
@@ -26,6 +50,7 @@ const CompanyForm = (props) => {
       </div>
       <div className="pt-9 pl-sm-9 pl-5 pr-sm-9 pr-5 pb-8 border-bottom border-width-1 border-default-color light-mode-texts">
         <div className="row pl-10 pr-10">
+        <FormProvider {...methods}>
           <div className="pb-10 col-12">
             <div className="col-12 text-center">
               <h4 className="font-size-6 font-weight-semibold mb-6">Company Image</h4>
@@ -46,66 +71,67 @@ const CompanyForm = (props) => {
           <div className="pb-10 col-6">
             <h4 className="font-size-6 font-weight-semibold mb-6">Company Name</h4>
             <div className="col-10 pl-0">
-              <input className="form-control" placeholder="Facebook..."/>
+              <TextField type="text" name="name" placeholder="Company name"/>
             </div>
           </div>
           <div className="pb-10 col-6">
             <h4 className="font-size-6 font-weight-semibold mb-6">Company Industry</h4>
             <div className="col-10 pl-0">
-              <input className="form-control" placeholder="Financial Industry..."/>
+              <TextField type="text" name="industry" placeholder="Financial Industry"/>
             </div>
           </div>
           <div className="pb-10 col-6">
             <h4 className="font-size-6 font-weight-semibold mb-6">Company Size</h4>
             <div className="col-10 pl-0">
-              <input className="form-control" placeholder="50-100"/>
+              <TextField type="text" name="size" placeholder="50-100"/>
             </div>
           </div>
           <div className="pb-10 col-6">
             <h4 className="font-size-6 font-weight-semibold mb-6">Company Founded</h4>
             <div className="col-10 pl-0">
-              <input className="form-control" placeholder="2010"/>
+              <TextField type="text" name="founded" placeholder="2010"/>
             </div>
           </div>
           <div className="pb-10 col-6">
             <h4 className="font-size-6 font-weight-semibold mb-6">Company Headquaters</h4>
             <div className="col-10 pl-0">
-              <ReactSelect />
+              <TextField type="text" name="address" placeholder="Address"/>
             </div>
           </div>
           <div className="pb-10 col-6">
             <h4 className="font-size-6 font-weight-semibold mb-6">Facebook</h4>
             <div className="col-10 pl-0">
-              <input className="form-control" placeholder="www.facebook.com"/>
+              <TextField type="text" name="facebook" placeholder="www.facebook.com"/>
             </div>
           </div>
           <div className="pb-10 col-6">
             <h4 className="font-size-6 font-weight-semibold mb-6">Linkedin</h4>
             <div className="col-10 pl-0">
-              <input className="form-control" placeholder="www.linkedin.com"/>
+              <TextField type="text" name="linkedin" placeholder="www.linkedin.com"/>
             </div>
           </div>
           <div className="pb-10 col-6">
             <h4 className="font-size-6 font-weight-semibold mb-6">Twitter</h4>
             <div className="col-10 pl-0">
-              <input className="form-control" placeholder="www.twitter.com"/>
+              <TextField type="text" name="twitter" placeholder="www.twitter.com"/>
             </div>
           </div>
           <div className="pb-10 col-6">
             <h4 className="font-size-6 font-weight-semibold mb-6">Company Website</h4>
             <div className="col-10 pl-0">
-              <input className="form-control" placeholder="www.company.com"/>
+              <TextField type="text" name="website" placeholder="www.company.com"/>
             </div>
           </div>
           <div className="pb-10 col-12">
             <h4 className="font-size-6 font-weight-semibold mb-6">Company Description</h4>
             <div className="col-12 pl-0">
-              <RichEditor />
+              <FormEditor name="description"/>
             </div>
           </div>
           <div className="col-12 my-15">
-            <a className="btn btn-primary btn-xl w-10 text-uppercase"><span className="mr-5 d-inline-block">+</span>Create company</a>
+            <button className="btn btn-primary btn-xl w-10 text-uppercase" onClick={methods.handleSubmit(onSubmit)}><span className="mr-5 d-inline-block">+</span>Create company</button>
           </div>
+          </FormProvider>
         </div>
       </div>
     </div>
